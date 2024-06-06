19cb080eec8dfe8510a609ad8604b5a119fe67c6
shaupert <sylvain.haupert@mnhn.fr>
2024-06-06 17:05:26 +0200

    [DEV] update figures

--- Evascape_scripts/figures.py
+++ Evascape_scripts/figures.py
@@ -15,16 +15,15 @@ from pathlib import Path
 
 from toolbox import waveread, addin, flatsound, show_spectrogram, bracket_ramp, rms_normalize, one_normalize, short
 from bird_behavior import empty_abundance_dataframe, bird_behavior, timeline_chart
-from assemblage import singing_session
+from assemblage import singing_session, assemblage, database
 
-SOUND_DIR = Path('../Evascape_soundfiles')
+temp_dir = Path("C:/Users/ecoac-field/OneDrive/Documents/Articles-Recherches/Reconstructor/Samples/temp")
+fig_dir = Path(r"C:\Users\ecoac-field\OneDrive\Documents\Articles-Recherches\Reconstructor\Figures\fig_20240604")
 
-fig_dir = Path('../Evascape_figures')
-
-normch1_path = SOUND_DIR / 'bird_channel.csv'
+normch1_path = temp_dir / 'normsong.csv'
 normch1_df = pd.read_csv(normch1_path, sep=';', index_col = 0)
 
-normch2_path = SOUND_DIR / 'background_channel.csv'
+normch2_path = temp_dir / 'normch2.csv'
 normch2_df = pd.read_csv(normch2_path, sep=';', index_col = 0)
 
 samprate = 44100
@@ -39,7 +38,14 @@ abundance_df.loc[species_names] = 1
 #%% BEHAVIOR
 #########
 random.seed(333)
-behavior_df = bird_behavior(normch1_df, abundance_df, d_min=1, d_max=1, recording_duration=duration)
+behavior_df = bird_behavior(normsong_df = normch1_df, 
+                            abundance_df = abundance_df, 
+                            d_min = 0, 
+                            d_max = 100,
+                            recording_duration = duration,  #behavior algorithm taken from Suzuki et al. 2012
+                            all_random_ist = False, # does not take Suzuki's algorithm and makes all intersing intervals random instead
+                            duplicate = False,
+                            samprate = samprate)
 timeline_fig = timeline_chart(behavior_df, y_size = 0.5)
 timeline_path = fig_dir / 'timeline_fig'
 timeline_fig.savefig(timeline_path)
@@ -115,7 +121,7 @@ save_path = fig_dir / 'turphi_propagated.wav'
 soundfile.write(save_path, turphi_propagated, samprate)
 
 
-#%% BIOPHONY ASSEMBLAGE
+#BIOPHONY ASSEMBLAGE
 #######
 chorus_list  = [fricoe_propagated, phycol_propagated, sylatr_propagated, turphi_propagated]
 
@@ -127,38 +133,46 @@ chorus_spectro = show_spectrogram(chorus, vmin = -70, vmax = -20, samprate = sam
 save_path = fig_dir / 'chorus.wav'
 soundfile.write(save_path, chorus, samprate)
 
+# #BIOPHONY REVERBERATION
+# #######
 
-#%% CHANNEL_2
+# biophony_reverberated = reverb(biophony_assemblage)
+# biophony_reverberated_spectro = show_spectrogram(biophony_reverberated, samprate)
+# save_path = fig_dir + '/biophony_reverberated.wav'
+# soundfile.write(save_path, biophony_reverberated, samprate)
+
+
+#CHANNEL_2
 #######
 #S4A03536_20190720_030000_norm.wav
-channel2_path = Path(r"../Evascape_soundfiles/background_channel/ambient_sound/S4A03536_20190720_030000_norm.wav")
+channel2_path = Path(r"C:\Users\ecoac-field\OneDrive\Documents\Articles-Recherches\Reconstructor\Samples\temp\norm_ch2\ambient_sound\S4A03536_20190720_030000_norm.wav")
 channel2 = waveread(channel2_path)
 channel2_spectro = show_spectrogram(channel2, vmin = -70, vmax = -20, samprate = samprate)
 
-#%% FINAL ASSEMBLAGE
+
+#FINAL ASSEMBLAGE
 ######
 
 final_assemblage = addin(base_sound = channel2, 
-                        added_sound = chorus, 
-                        time_code = 0, 
-                        ramp_duration = 0, sr = samprate)
+                       added_sound = chorus, 
+                       time_code = 0, 
+                       ramp_duration = 0, sr = samprate)
 final_assemblage = bracket_ramp(final_assemblage)
 final_assemblage_spectro = show_spectrogram(final_assemblage, vmin = -70, vmax = -20, samprate = samprate)
 save_path = fig_dir / 'final_assemblage.wav'
 soundfile.write(save_path, final_assemblage, samprate)
 
 norm_assemblage = one_normalize(final_assemblage)
-norm_assemblage_spectro = show_spectrogram(norm_assemblage, vmin = -70, vmax = -20, samprate = samprate)
+norm_assemblage_spectro = show_spectrogram(norm_assemblage, vmin = -50, vmax = -20, samprate = samprate)
 save_path = fig_dir / 'norm_assemblage_far2.wav'
 soundfile.write(save_path, norm_assemblage, samprate)
 
-assemblage_sample = short(norm_assemblage, start = 25, end = 35)  
-norm_assemblage_spectro = show_spectrogram(assemblage_sample, vmin = -70, vmax = -20, samprate = samprate)  
+assemblage_sample = short(norm_assemblage, start = 25, end = 35)    
 save_path = fig_dir / 'norm_assemblage_sample.wav'  
 soundfile.write(save_path, assemblage_sample, samprate)  
 
 
-#%% TIMELINE VS SPECTROGRAM
+# TIMELINE VS SPECTROGRAM
 ######   
 
 fig, axs = plt.subplots(nrows = 1, ncols = 2, figsize=(30,15))
@@ -169,17 +183,16 @@ plt.figure(timeline_fig)
 axs[0].set_xlabel('Time [s]', size = 20)
 
 #spectrogram
-fig_spectro, axs_spectro = plt.subplots(nrows = 4, ncols = 1)
+fig, axs = plt.subplots(nrows = 2, ncols = 1)
 
-# plot the figure fricoe_spectrogram in the first ax
-plt.sca(axs_spectro[0])
-plt.imshow(fricoe_spectrogram)   
-fricoe_spectrogram
-axs_spectro[0].imshow(fricoe_spectrogram)   
-axs_spectro[0] = fricoe_spectrogram
-axs_spectro[1] = phycol_spectrogram
-axs_spectro[2] = sylatr_spectrogram
-axs_spectro[3] = turphi_spectrogram
+
+axs_spectro[0].imshow(fricoe_spectrogram)
+
+axs_spectro[1].imshow(phycol_spectrogram)
+
+axs_spectro[2].imshow(sylatr_spectrogram)
+
+axs_spectro[3].imshow(turphi_spectrogram)
 
 plt.figure(fig_spectro)
 axs[1].set_title('singing session spectrograms', size=20)
@@ -199,31 +212,32 @@ N = 4096
 vmin = None
 vmax = None
 
+
 fig, axs = plt.subplots(nrows = 2, ncols = 1, figsize=(30,15))
 
-fricoe_Sxx_power,tn,fn,ext = sound.spectrogram(fricoe_session, 44100, nperseg=N, noverlap=N//2, flims = [0, 15000], mode = 'amplitude')
+fricoe_Sxx_power,tn,fn,ext = maad.sound.spectrogram(fricoe_session, 44100, nperseg=N, noverlap=N//2, flims = [0, 15000], mode = 'amplitude')
 fig_kwargs = {'vmax': vmax,
-            'vmin':vmin,
-            'extent':ext,
-            'figsize':(4,13),
-            'title':'Power spectrogram density (PSD)',
-            'xlabel':'Time [sec]',
-            'ylabel':'Frequency [Hz]',
-            }
-fricoe_Sxx_dB = util.power2dB(fricoe_Sxx_power) # convert into dB
-util.plot2d(fricoe_Sxx_dB,**fig_kwargs, ax = axs[0])
+                  'vmin':vmin,
+                  'extent':ext,
+                  'figsize':(4,13),
+                  'title':'Power spectrogram density (PSD)',
+                  'xlabel':'Time [sec]',
+                  'ylabel':'Frequency [Hz]',
+                  }
+fricoe_Sxx_dB = maad.util.power2dB(fricoe_Sxx_power) # convert into dB
+maad.util.plot2d(fricoe_Sxx_dB,**fig_kwargs, ax = axs[0])
 
-phycol_Sxx_power,tn,fn,ext = sound.spectrogram(phycol_session, 44100, nperseg=N, noverlap=N//2, flims = [0, 15000], mode = 'amplitude')
+phycol_Sxx_power,tn,fn,ext = maad.sound.spectrogram(phycol_session, 44100, nperseg=N, noverlap=N//2, flims = [0, 15000], mode = 'amplitude')
 fig_kwargs = {'vmax': vmax,
-            'vmin':vmin,
-            'extent':ext,
-            'figsize':(4,13),
-            'title':'Power spectrogram density (PSD)',
-            'xlabel':'Time [sec]',
-            'ylabel':'Frequency [Hz]',
-            }
-phycol_Sxx_dB = util.power2dB(phycol_Sxx_power) # convert into dB
-util.plot2d(phycol_Sxx_dB,**fig_kwargs, ax = axs[1])
+                  'vmin':vmin,
+                  'extent':ext,
+                  'figsize':(4,13),
+                  'title':'Power spectrogram density (PSD)',
+                  'xlabel':'Time [sec]',
+                  'ylabel':'Frequency [Hz]',
+                  }
+phycol_Sxx_dB = maad.util.power2dB(phycol_Sxx_power) # convert into dB
+maad.util.plot2d(phycol_Sxx_dB,**fig_kwargs, ax = axs[1])
 
 # Comparaison ISTI
 observed_ISTI_path = temp_dir / 'temporal_analysis.csv'
@@ -255,7 +269,7 @@ for species in species_list:
 plt.boxplot(toplot_list, labels = label_list)
 plt.xticks(rotation = 90)
 
-#%% COMPARAISON AVEC ANNOTATION JB
+#COMPARAISON AVEC ANNOTATION JB
 #####
 
 # On compare avec un enregistrement qu'il a annoté : S4A03536_20190624_103000.wav
@@ -316,7 +330,7 @@ soundfile.write(save_path, basic_sample, samprate)
 
 
 
-#%% PSYCHOAC
+# PSYCHOAC
 
 # background power spectrum
 background_list = ['ambient_sound', 'rain_pw02', 'aircraft_pw02']
@@ -330,8 +344,8 @@ for i, background in enumerate(background_list):
     add_power_spectro = np.array([0 for i in range(512)])
     for sound_file in background_df.index:
         signal = waveread(background_df.fullfilename[sound_file])
-        Sxx_power,tn, fn, ext = sound.spectrogram (signal, samprate, mode="amplitude")
-        power_spectro = sound.avg_amplitude_spectro(Sxx_power)
+        Sxx_power,tn, fn, ext = maad.sound.spectrogram (signal, samprate, mode="amplitude")
+        power_spectro = maad.sound.avg_amplitude_spectro(Sxx_power)
         add_power_spectro = np.add(add_power_spectro, power_spectro)
     avg_power_spectro = add_power_spectro/len(background_df)
     plt.sca(axs[i])
